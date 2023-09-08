a50aa2486332348e6a4011710bea0319dcfc80e4
+ <kingoriperis@gmail.com>
2023-09-08 15:53:58 +0300

    completed assignment

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Filter({ onCategoryChange }) {
+
+function Filter({ onCategoryChange, onSearchChange }) {
+  const [searchText ,setSearchText]= useState("");
+  const handleSearchChange=(event)=>{
+    const newText = event.target.value;
+    setSearchText(newText);
+    onSearchChange(newText);
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" name="search" placeholder="Search..." value={searchText} 
+        onChange={handleSearchChange}/>
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
